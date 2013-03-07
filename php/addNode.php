27fc1e4a87be05bba74aa05d7ca594a0b37d6458
Libor Vohanka <vohanka.libor@gmail.com>
2013-03-07 12:02:41 +0100

    edit - data layer was separated by moving sql queries to jqTagTree.db.class.php

--- php/addNode.php
+++ php/addNode.php
@@ -1,24 +1,18 @@
 <?php
-include 'db.php';
+include 'jqTagTree.db.class.php';
 include '../config/constants.php';
 
 $connectDb = Db::getInstance();
 
-$id = $_GET['id'];
-if($id==0){
-	//$roots = $connectDb->readFromDb('SELECT a.id, a.name, a.uri FROM cfp_automatic_tags a WHERE a.id NOT IN (SELECT b.subject_id FROM cfp_automatic_tags_triples b)');
-	$roots = $connectDb->readFromDb('SELECT a.id, a.name, a.uri, count(c.id) AS children FROM cfp_automatic_tags a LEFT JOIN cfp_automatic_tags_triples c ON a.id=c.object_id 
-  									WHERE a.id NOT IN (SELECT b.subject_id FROM cfp_automatic_tags_triples b) GROUP BY a.id');
+$data = $_GET['data'];
+//								name , uri
+$result = $connectDb->addTag($data[1], $data[2]);
+$return['id'] = mysql_insert_id();
+
+if ($result){//								id  ,id of added tag
+	$result = $connectDb->addTagConnect($data[0], $return['id'], JQTT_CONNECTIONS_TABLE_DEFAULT_PREDICATE);
 }else{
-	//$roots = $connectDb->readFromDb('SELECT a.id, a.name, a.uri FROM cfp_automatic_tags a WHERE id IN( SELECT b.subject_id FROM cfp_automatic_tags_triples b WHERE b.object_id = ('.$id.'))');
-	$roots = $connectDb->readFromDb('SELECT a.id, a.name, a.uri, count(c.id) AS children FROM cfp_automatic_tags a LEFT JOIN cfp_automatic_tags_triples c ON a.id=c.object_id 
-  									WHERE a.id IN( SELECT b.subject_id FROM cfp_automatic_tags_triples b WHERE b.object_id='.$id.') GROUP BY a.id');
+	echo "Unable to write to database";
 }
 
-for ($i=0;$i<count($roots); $i++) {
-	$numberOfChildren = $connectDb->numberOfRows('SELECT subject_id FROM cfp_automatic_tags_triples WHERE object_id = '.$roots[$i]['id']);
-	$roots[$i]['child'] = $numberOfChildren;
-	$numberOfChildren = 0;
-}
-
-echo json_encode( $roots );
+echo json_encode($return);
