3e02ab3c533d4757c46caad1e66783460267affc
Libor Vohanka <vohanka.libor@gmail.com>
2013-03-07 12:04:27 +0100

    edit - data layer was separated by adding sql queries edit - changed name of file

--- php/jqTagTree.db.class.php
+++ php/jqTagTree.db.class.php
@@ -41,33 +41,33 @@ class Db{
 	public function setCharset($charset){
 		return mysql_set_charset($charset);
 	}
-	public function escape($string){
+	private function escape($string){
 		return mysql_real_escape_string($string);
 	}
 	//Dotaz do DB
 	public function query($dotaz){
 		return mysql_query($dotaz);
 	}
-//Databázová funkce počet řádek
-	public function numRows($dotaz){
+	//Databázová funkce počet řádek
+	private function numRows($dotaz){
 		return mysql_num_rows($dotaz);
 	}
 	//Vrací řádek ze zadaného dotazu
-	public function fetchArray($dotaz){
+	private function fetchArray($dotaz){
 		return mysql_fetch_array($dotaz,MYSQL_ASSOC);
 	}
 	//Funkce vracející počet řádek ze zadaného dotazu
-	public function numberOfRows($dotaz){
+	private function numberOfRows($dotaz){
 		$vysledek = $this->query($dotaz);
 		return $this->numRows($vysledek);
 	}
 	//Funkce vracející jeden řádek ze zadaného dotazu
-	public function readLine($dotaz){
+	private function readLine($dotaz){
 		$vysledek = $this->query($dotaz);
 		return $this->fetchArray($vysledek);
 	}
 	//Funkce vracející dvojrozměrné pole řádek v databázi ze zadaného dotazu
-	public function readFromDb($dotaz){
+	private function readFromDb($dotaz){
 		$vsechnyRadek = NULL;
 		$vysledek = $this->query($dotaz);
 		$pocetRadek = $this->numRows($vysledek);
@@ -76,7 +76,45 @@ class Db{
 		}
 		return $vsechnyRadek;
 	}
+	//loading first level of tree
+	public function loadFirstLevelFromDb(){
+		$record = $this->readFromDb('SELECT a.'. JQTT_TAG_TABLE_ID.', a.'. JQTT_TAG_TABLE_NAME .', a.'. JQTT_TAG_TABLE_URI .', count(c.'. JQTT_TAG_TABLE_ID.') AS children FROM '. JQTT_TAG_TABLE .' a 
+		LEFT JOIN ' . JQTT_CONNECTIONS_TABLE . ' c ON a.'. JQTT_TAG_TABLE_ID.'=c.'. JQTT_CONNECTIONS_TABLE_PARENT.'	WHERE a.'. JQTT_TAG_TABLE_ID .' NOT IN (SELECT b.'. JQTT_CONNECTIONS_TABLE_CHILD .' 
+		FROM '. JQTT_CONNECTIONS_TABLE .' b) GROUP BY a.'. JQTT_TAG_TABLE_ID .' order by a.'.JQTT_TAG_TABLE_NAME);
+		
+		return $record;
+	}
 
+	//loading sub tree of passed id
+	public function loadNextLevelFromDb($id){
+		$id = $this->escape($id);
+		$record = $this->readFromDb('SELECT a.'. JQTT_TAG_TABLE_ID.', a.'. JQTT_TAG_TABLE_NAME .', a.'. JQTT_TAG_TABLE_URI .', count(c.'. JQTT_TAG_TABLE_ID.') AS children FROM '. JQTT_TAG_TABLE .' a 
+		LEFT JOIN ' . JQTT_CONNECTIONS_TABLE . ' c ON a.'. JQTT_TAG_TABLE_ID.'=c.'. JQTT_CONNECTIONS_TABLE_PARENT.'	WHERE a.'. JQTT_TAG_TABLE_ID .' IN (SELECT b.'. JQTT_CONNECTIONS_TABLE_CHILD .' 
+		FROM '. JQTT_CONNECTIONS_TABLE .' b WHERE b.'.JQTT_CONNECTIONS_TABLE_PARENT.'='.$id.') GROUP BY a.'.JQTT_TAG_TABLE_NAME);
+		
+		return $record;
+	}
+	
+	public function editTag($id, $newName, $newUri){
+		$escId = $this->escape($id);
+		$escName = $this->escape($newName);
+		$escUri = $this->escape($newUri);
+		return $this->query("UPDATE " . JQTT_TAG_TABLE . " SET " . JQTT_TAG_TABLE_NAME . " = '" . $escName . "', " . JQTT_TAG_TABLE_URI . " = '" . $escUri . "' WHERE " . JQTT_TAG_TABLE_ID . "= '" . $escId . "'");
+	}
+	
+	public function addTag($name, $uri){
+		$escName = $this->escape($name);
+		$escUri = $this->escape($uri);
+		return $this->query("INSERT INTO " . JQTT_TAG_TABLE . " ( " . JQTT_TAG_TABLE_NAME . ", ". JQTT_TAG_TABLE_URI .") VALUES ('" . $escName . "','" . $uri . "') ");
+	}
+	
+	public function addTagConnect($parent, $child, $predicate){
+		$escParent = $this->escape($parent);
+		$escChild = $this->escape($child);
+		return $this->query("INSERT INTO " . JQTT_CONNECTIONS_TABLE . " ( " . JQTT_CONNECTIONS_TABLE_PARENT . ", ". JQTT_CONNECTIONS_TABLE_CHILD .", ". JQTT_CONNECTIONS_TABLE_PREDICATE .") 
+																	VALUES ('" . $escParent . "','" . $escChild . "','".$predicate."') ");
+	}
+	
 }
 
 ?>
