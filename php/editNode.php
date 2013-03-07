5db1258d333acc6e33f3d76a80ce580e09c4e21c
Libor Vohanka <vohanka.libor@gmail.com>
2013-03-07 12:03:10 +0100

    edit - data layer was separated by moving sql queries to jqTagTree.db.class.php

--- php/editNode.php
+++ php/editNode.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'db.php';
+include 'jqTagTree.db.class.php';
 include '../config/constants.php';
 
 $connectDb = Db::getInstance();
@@ -9,11 +9,8 @@ $data = $_GET['data'];
 
 $result = "false";
 
-if (($data[1] != $data[2]) && ($data[3] != $data[4])) {
-	$result = $connectDb->query("UPDATE " . JQTT_TAG_TABLE . " SET " . JQTT_TAG_TABLE_NAME . " = '" . $data[2] . "', " . JQTT_TAG_TABLE_URI . " = '" . $data[4] . "' WHERE " . JQTT_TAG_TABLE_ID . "= '" . $data[0] . "'");
-} else if (($data[1] != $data[2])) {
-	$result = $connectDb->query("UPDATE " . JQTT_TAG_TABLE . " SET " . JQTT_TAG_TABLE_NAME . " = '" . $data[2] . "' WHERE " . JQTT_TAG_TABLE_ID . "= '" . $data[0] . "'");
-} elseif (($data[3] != $data[4])) {
-	$result = $connectDb->query("UPDATE " . JQTT_TAG_TABLE . " SET " . JQTT_TAG_TABLE_URI . " = '" . $data[4] . "' WHERE " . JQTT_TAG_TABLE_ID . " = '" . $data[0] . "'");
+if (($data[1] != $data[2]) || ($data[3] != $data[4])) {
+	$result = $connectDb->editNode($data[0], $data[2], $data[4]);
 }
+
 echo json_encode($result);
